bb20e94dc51fb2325c1300f3482740f818541990
Oscar Rijpstra <00276969@rijnijssel.nl>
2019-08-23 16:56:09 +0200

    moved imgs

--- js/slider.js
+++ js/slider.js
@@ -3,7 +3,7 @@ sliders = [{name: 'IMG_4533h7oaff', alt: 'voorkant Garage Sukkel'}, {name: 'IMG_
 imgExtentions = ['webp', 'wdp' ,'jp2', 'jpg'],
 desktopResolutions = [1000, 1200, 1400, 1600, 1800, 2000],
 mobileResolutions = [{width: 300, height: 200}, {width: 500, height: 400}, {width: 700, height: 600}, {width:900, height: 800}],
-imageLocation = {baseUrl: 'https://res.cloudinary.com/dyubvu51u/image/upload/', cloudName: 'v1562311977'};
+imageLocation = {baseUrl: 'https://res.cloudinary.com/dyubvu51u/image/upload/', cloudName: 'v1562311989'};
 
 let currentslider = 0;
 
@@ -25,7 +25,7 @@ function addSliders(container) {
             sourceDesktop.type = 'image/' + extention;
 
             desktopResolutions.map((resolution, k) => {
-                sourceDesktop.srcset += `${imageLocation.baseUrl}c_fill,q_80,w_${resolution},h_800/${imageLocation.cloudName}/${slider.name}.${extention} ${resolution}w`;
+                sourceDesktop.srcset += `${imageLocation.baseUrl}c_fill,q_80,w_${resolution},h_800/${imageLocation.cloudName}/Garage%20Sukkel/${slider.name}.${extention} ${resolution}w`;
                 if (k < desktopResolutions.length - 1) sourceDesktop.srcset += ',';
             })
 
@@ -35,14 +35,14 @@ function addSliders(container) {
             sourceMobile.type = 'image/' + extention;
 
             mobileResolutions.map((resolution, k) => {
-                sourceMobile.srcset += `${imageLocation.baseUrl}c_fill,q_80,w_${resolution.width},h_${resolution.height}/${imageLocation.cloudName}/${slider.name}.${extention} ${resolution.width}w`;
+                sourceMobile.srcset += `${imageLocation.baseUrl}c_fill,q_80,w_${resolution.width},h_${resolution.height}/${imageLocation.cloudName}/Garage%20Sukkel/${slider.name}.${extention} ${resolution.width}w`;
                 if (k < mobileResolutions.length - 1) sourceMobile.srcset += ',';
             })
         })
 
         const jpgFallback = document.createElement('img');
         picture.appendChild(jpgFallback);
-        jpgFallback.src = `${imageLocation.baseUrl}'c_fill,w_1080/${imageLocation.cloudName}/${slider.name}.jpg`;
+        jpgFallback.src = `${imageLocation.baseUrl}'c_fill,w_1080/${imageLocation.cloudName}/Garage%20Sukkel/${slider.name}.jpg`;
         jpgFallback.alt = slider.alt;
         jpgFallback.classList.add('slider__item');
         if (i == 0) jpgFallback.classList.add('new');
